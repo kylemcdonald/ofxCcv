f03c88b4d56c227724626bc5dcb02e832d069066
Gene Kogan <kogan.gene@gmail.com>
2015-11-11 21:23:34 -0500

    added mkdir to model downloader

---
+++ download-classifier.sh
@@ -0,0 +1,9 @@
+mkdir example/bin
+mkdir example/bin/data
+
+curl -o \
+	example/bin/data/image-net-2012.sqlite3 \
+	https://raw.githubusercontent.com/liuliu/ccv/unstable/samples/image-net-2012.sqlite3
+curl -o \
+	example/bin/data/image-net-2012.words \
+	https://raw.githubusercontent.com/liuliu/ccv/unstable/samples/image-net-2012.words
