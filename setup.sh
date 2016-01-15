0aece81d9f295877780f89c0d8bfc04791cb68c9
Kyle McDonald <kyle@kylemcdonald.net>
2016-01-15 22:31:49 +0900

    better script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p example/bin
-mkdir -p example/bin/data
-
+DIR=example/bin/data
 DB_URL="https://raw.githubusercontent.com/liuliu/ccv/unstable/samples/image-net-2012.sqlite3"
-DB_FILE="example/bin/data/image-net-2012.sqlite3"
+DB_FILE="$DIR/image-net-2012.sqlite3"
 WORDS_URL="https://raw.githubusercontent.com/liuliu/ccv/unstable/samples/image-net-2012.words"
-WORDS_FILE="example/bin/data/image-net-2012.words"
+WORDS_FILE="$DIR/image-net-2012.words"
+mkdir -p $DIR
 if [ ! -e $DB_FILE ] ; then
 	echo "Downloading image-net-2012 network (ccv)"
 	curl -o $DB_FILE $DB_URL
