07a2521bd68ff10a3c44493f45243c7e9907431e
Gene Kogan <kogan.gene@gmail.com>
2016-02-11 13:21:36 +0530

    updated download script

--- setup.sh
+++ setup.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-DIR=example-classify/bin/data
-DIR2=example-encode/bin/data
-DIR3=example-viewer/bin/data
+DIR="example-classify/bin/data/"
+DIR2="example-encode/bin/data/"
+DIR3="example-viewer/bin/data/"
+
 DB_URL="https://raw.githubusercontent.com/liuliu/ccv/unstable/samples/image-net-2012.sqlite3"
-DB_FILE="$DIR/image-net-2012.sqlite3"
-DB_FILE2="$DIR2/image-net-2012.sqlite3"
-DB_FILE3="$DIR3/image-net-2012.sqlite3"
 WORDS_URL="https://raw.githubusercontent.com/liuliu/ccv/unstable/samples/image-net-2012.words"
-WORDS_FILE="$DIR/image-net-2012.words"
-WORDS_FILE2="$DIR2/image-net-2012.words"
-WORDS_FILE3="$DIR3/image-net-2012.words"
+
+DB_FILE="image-net-2012.sqlite3"
+WORDS_FILE="image-net-2012.words"
+
 mkdir -p $DIR
 mkdir -p $DIR2
 mkdir -p $DIR3
 if [ ! -e $DB_FILE ] ; then
-	echo "Downloading image-net-2012 network (ccv)"
-	curl -o $DB_FILE $DB_URL
-	curl -o $WORDS_FILE $WORDS_URL
-	cp $DB_FILE $DB_FILE2
-	cp $DB_FILE $DB_FILE3
-    cp $WORDS_FILE $WORDS_FILE2
-    cp $WORDS_FILE $WORDS_FILE3
+echo "Downloading image-net-2012 network (ccv)"
+curl -o $DIR$DB_FILE $DB_URL
+curl -o $DIR$WORDS_FILE $WORDS_URL
+cp $DIR$DB_FILE $DIR2$DB_FILE
+cp $DIR$DB_FILE $DIR3$DB_FILE
+cp $DIR$WORDS_FILE $DIR2$WORDS_FILE
+cp $DIR$WORDS_FILE $DIR3$WORDS_FILE
 fi
 echo "Downloaded: image-net-2012 network (ccv)"
